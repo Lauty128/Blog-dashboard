b5c9ba15b767673de6285e0d31ca72b3e3ccf255
Lauty128 <lautarosilverii@gmail.com>
2023-07-02 18:36:47 -0300

    squeleton added in articles loading

--- src/components/ArticlesList.tsx
+++ src/components/ArticlesList.tsx
@@ -47,14 +47,12 @@ export function ArticlesList(){
             <h1 className="Home__h1">LISTA DE ARTICULOS</h1>
             
             <div className="Home__articlesContainer">
+                <Link to={"/create"} className="Home__createButton">
+                    <svg width="24px" height="24px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#616161"><path d="M8 12h4m4 0h-4m0 0V8m0 4v4M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" stroke="#616161" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    AGREGAR ARTICULO
+                </Link>
                 { (articles !== null)
-                    ?   <>
-                            <Link to={"/create"} className="Home__createButton">
-                                <svg width="24px" height="24px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#616161"><path d="M8 12h4m4 0h-4m0 0V8m0 4v4M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" stroke="#616161" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-                                AGREGAR ARTICULO
-                            </Link>
-                            { articles.map(article=> ArtilceCard(article) ) }
-                        </>
+                    ? articles.map(article=> ArtilceCard(article) ) 
                     : PreLoad() 
                 }
                 <Pagination />
